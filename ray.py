b26a03d9644bc66ad5e133b8ff3a143f66b8a9f9
BastiHz <41594266+BastiHz@users.noreply.github.com>
2019-05-07 22:59:37 +0200

    Let the ray connect to the nearest wall in ray direction.

--- ray.py
+++ ray.py
@@ -9,7 +9,7 @@ class Ray:
         self.dy = math.sin(angle)
         self.x2 = self.x1 + self.dx
         self.y2 = self.y1 + self.dy
-        self.color = (255, 128, 0)
+        self.color = (255, 196, 0)
         self.wall_intersect = []
 
     def update_position(self, pos):
@@ -25,28 +25,28 @@ class Ray:
                 (self.x1, self.y1),
                 self.wall_intersect
             )
-            # pg.draw.circle(
-            #     target_surf,
-            #     self.color,
-            #     (int(self.wall_intersect[0]),
-            #      int(self.wall_intersect[1])),
-            #     5
-            # )
 
-    def cast(self, wall):
+    def cast(self, walls):
         self.wall_intersect = ()
-        denominator = ((wall.x1 - wall.x2) * (self.y1 - self.y2)
-                       - (wall.y1 - wall.y2) * (self.x1 - self.x2))
-        if denominator == 0:
-            return
-        t = (((wall.x1 - self.x1) * (self.y1 - self.y2)
-              - (wall.y1 - self.y1) * (self.x1 - self.x2)) / denominator)
-        u = -((wall.x1 - wall.x2) * (wall.y1 - self.y1)
-              - (wall.y1 - wall.y2) * (wall.x1 - self.x1)) / denominator
-        if 0 < t < 1 and u > 0:
-            self.wall_intersect = (
-                    wall.x1 + t * (wall.x2 - wall.x1),
-                    wall.y1 + t * (wall.y2 - wall.y1)
-            )
-
-        # FIXME: Determine closes wall, not last tested. Use only closest for self.wall_intersect.
+        min_distance = math.inf
+        for w in walls:
+            denominator = ((w.x1 - w.x2) * (self.y1 - self.y2)
+                           - (w.y1 - w.y2) * (self.x1 - self.x2))
+            if denominator == 0:
+                continue
+            t = (((w.x1 - self.x1) * (self.y1 - self.y2)
+                  - (w.y1 - self.y1) * (self.x1 - self.x2)) / denominator)
+            u = -((w.x1 - w.x2) * (w.y1 - self.y1)
+                  - (w.y1 - w.y2) * (w.x1 - self.x1)) / denominator
+            if 0 < t < 1 and u > 0:
+                w_intersect = (
+                    w.x1 + t * (w.x2 - w.x1),
+                    w.y1 + t * (w.y2 - w.y1)
+                )
+                distance = math.hypot(
+                    self.x1 - w_intersect [0],
+                    self.y1 - w_intersect [1]
+                )
+                if distance < min_distance:
+                    min_distance = distance
+                    self.wall_intersect = w_intersect
--- top_view_scene.py
+++ top_view_scene.py
@@ -1,4 +1,5 @@
 import math
+import random
 import pygame as pg
 from ray import Ray
 from wall import Wall
@@ -7,18 +8,27 @@ from wall import Wall
 class TopView:
     def __init__(self, app):
         self.app = app
-        self.ray_origin_pos = (0, 0)
+        self.color = (255, 196, 0)
+        self.background_color = (20, 20, 20)
+        self.ray_origin = (0, 0)
         self.rays = []
-        for a in range(0, 360, 1):
-            # print(a)
-            self.rays.append(Ray(self.ray_origin_pos, math.radians(a)))
+        for angle in range(0, 360, 5):
+            self.rays.append(Ray(self.ray_origin, math.radians(angle)))
         self.walls = [
-            Wall(600, 200, 500, 500),
-            Wall(0, 0, 800-2, 0),  # 2 = hardcoded wall thickness
-            Wall(0, 0, 0, 600-2),  # '-2' makes left and lower wall visible
-            Wall(800-2, 0, 800-2, 600-2),
-            Wall(0, 600-2, 800-2, 600-2)
+            # Wall(600, 200, 500, 500),
+            Wall(0, 0, 800, 0),
+            Wall(0, 0, 0, 600),
+            Wall(800-2, 0, 800-2, 600),  # 2 = hardcoded wall thickness
+            Wall(0, 600-2, 800, 600-2)   # '-2' makes left and lower wall visible
         ]
+        w, h = self.app.main_surface.get_rect().size
+        for _ in range(5):
+            self.walls.append(Wall(
+                random.randint(0, w),
+                random.randint(0, h),
+                random.randint(0, w),
+                random.randint(0, h)
+            ))
 
     def handle_input(self, event):
         if event.type == pg.KEYDOWN:
@@ -26,15 +36,15 @@ class TopView:
                 self.app.running = False
 
     def update(self):
-        mouse_pos = pg.mouse.get_pos()
+        self.ray_origin = pg.mouse.get_pos()
         for r in self.rays:
-            r.update_position(mouse_pos)
-            for w in self.walls:
-                r.cast(w)
+            r.update_position(self.ray_origin)
+            r.cast(self.walls)
 
     def draw(self, target_surf):
-        target_surf.fill((0, 0, 0))
-        for w in self.walls:
-            w.draw(target_surf)
+        target_surf.fill(self.background_color)
+        # pg.draw.circle(target_surf, self.color, self.ray_origin, 3)
         for r in self.rays:
             r.draw(target_surf)
+        for w in self.walls:
+            w.draw(target_surf)
